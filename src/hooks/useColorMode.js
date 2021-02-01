3e491a26b8ebca5b0f1205bce2cd105325049b67
dcwds <dcwds@protonmail.com>
2021-01-31 23:21:04 -0500

    fix(color mode): prevent color flickering on initial load and untrack public dir

--- src/hooks/useColorMode.js
+++ src/hooks/useColorMode.js
@@ -1,32 +1,7 @@
 import { useEffect, useState } from "react"
 
-export const MODE_DARK = "dark"
-export const MODE_LIGHT = "light"
-
-const getColorMode = () => {
-  // Check for window so Gatsby doesn't fail on build.
-  if (typeof window !== "undefined") {
-    // Check local storage for color mode.
-    const persistedColorMode = window.localStorage.getItem("color-mode")
-
-    if (persistedColorMode) {
-      return persistedColorMode
-    }
-
-    // Check for color mode OS preference.
-    const mql = window.matchMedia("(prefers-color-scheme: dark)")
-
-    if (typeof mql.matches === "boolean") {
-      return mql.matches ? MODE_DARK : MODE_LIGHT
-    } else {
-      // Use dark color mode as default.
-      return MODE_DARK
-    }
-  }
-}
-
 const useColorMode = () => {
-  const [colorMode, setColorMode] = useState(getColorMode)
+  const [colorMode, setColorMode] = useState("dark")
 
   useEffect(() => {
     // Check for window so Gatsby doesn't fail on build.
@@ -35,12 +10,10 @@ const useColorMode = () => {
       window.localStorage.setItem("color-mode", colorMode)
     }
 
-    // Add appropriate color class to HTML tag.
     document.documentElement.classList.add(colorMode)
 
-    // Remove opposite color class from HTML tag.
     document.documentElement.classList.remove(
-      colorMode === MODE_DARK ? MODE_LIGHT : MODE_DARK
+      colorMode === "dark" ? "light" : "dark"
     )
   }, [colorMode, setColorMode])
 
---
+++ src/html.js
@@ -0,0 +1,63 @@
+import React from "react"
+import PropTypes from "prop-types"
+
+export default function HTML(props) {
+  return (
+    <html {...props.htmlAttributes}>
+      <head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        {props.headComponents}
+      </head>
+      <body {...props.bodyAttributes}>
+        <script
+          key="dcwds-theme"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function () {
+                try {
+                  const persistedColorMode = window.localStorage.getItem(
+                    "color-mode"
+                  )
+                  const preferredColorMode = window.matchMedia(
+                    "(prefers-color-scheme: dark)"
+                  ).matches
+                    ? "dark"
+                    : "light"
+
+                  if (persistedColorMode) {
+                    document.documentElement.classList.add(persistedColorMode)
+                  } else if (!persistedColorMode && preferredColorMode) {
+                    document.documentElement.classList.add(preferredColorMode)
+                  } else {
+                    document.documentElement.classList.add("dark")
+                  }
+                } catch (e) {}
+              })()
+            `,
+          }}
+        />
+        {props.preBodyComponents}
+        <div
+          key={`body`}
+          id="___gatsby"
+          dangerouslySetInnerHTML={{ __html: props.body }}
+        />
+        {props.postBodyComponents}
+      </body>
+    </html>
+  )
+}
+
+HTML.propTypes = {
+  htmlAttributes: PropTypes.object,
+  headComponents: PropTypes.array,
+  bodyAttributes: PropTypes.object,
+  preBodyComponents: PropTypes.array,
+  body: PropTypes.string,
+  postBodyComponents: PropTypes.array,
+}
